97c45e4b8138a4a266cc0ffe075584ca7e73c3ea
Picanha_01 <johnyjony76@gmail.com>
2025-05-31 16:45:21 +0100

    add codigos e outros

---
+++ PROJECO_P2_2/QuestaoExame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECO_P2_2
+{
+    class QuestaoExame
+    {
+        public int Id { get; set; }
+        public string Pergunta { get; set; }
+        public string OpcaoA { get; set; }
+        public string OpcaoB { get; set; }
+        public string OpcaoC { get; set; }
+        public string OpcaoD { get; set; }
+        public string Disciplina { get; set; }
+    }
+}
